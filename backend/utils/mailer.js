a2bfd8befdec6ad3ce6a4c3dfc1eea25a05c5cf5
abinand <abinand705@gmail.com>
2026-08-03 20:00:17 +0530

    feat: implement nodemailer transporter and sendMail utility function

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -2,11 +2,14 @@ const nodemailer = require("nodemailer");
 
 // Create a transporter using Gmail service
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4,
 });
 
 /**
